9127e5af46488c8899dace072061dd27b2ffd89a
aquiles <eu@email.com>
2024-08-26 20:49:02 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,10 @@
+nodule.exports = {
+    blog: {
+        username: 'postgres',
+        port:  '5432',
+        database: 'blog',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+        logging: false
+    }
+} 
