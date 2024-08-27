8434b59d13613acb016fadaae7fba41da31954fd
aquiles <eu@email.com>
2024-08-26 21:22:24 -0300

    second-commit

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 const  { createServer } = require('http');
-const express = require ('express');
-
+// const express = require ('express');
 const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
 
-let app = express();
+// const bodyParser = require('body-parser');
 
 dotenv.config();  // procura o arquivo .env e le
+// let app = express();
+const app = require('./config/router-factory');
 
-app.use(bodyParser.json());
-
+// app.use(bodyParser.json());
+const http = createServer(app);
 //app.use(express.json()); // funciona dos 2 jeitos
 
 process.on('SIGINT', () => http.close((error) => {
     if(error){
         console.log(`${error.name}: ${error.message}`)
     }
-
     process.exit(error ? 1:0);
-}))
+}));
 
-
-
-const http = createServer(app);
-
-app.get('/', (req, res) => res.send('<h1>Resposta</h1>'));
-
-app.post('/', function (req, res){
-    const name = req.body.name;
-    res.send(`Olá ${name}`);
-})
+// const http = createServer(app);
+// app.get('/', (req, res) => res.send('<h1>Resposta</h1>'));
 
 http.listen(8989, () => console.log('Listening to port 8989'));
