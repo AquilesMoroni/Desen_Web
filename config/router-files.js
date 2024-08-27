8416e6092de37da494bd7bda82f502cf1862b387
aquiles <eu@email.com>
2024-08-26 21:53:17 -0300

    terceiro-commit

---
+++ config/router-files.js
@@ -0,0 +1,18 @@
+const filenames = {
+    'blog/api/v1/rest': [
+        'post-resource',
+        'reply-resource',
+    ],
+};
+
+function toFilesList(imports, folder) {
+    return [...imports, ...filenames[folder].map(toRelativePaths(folder))];
+}
+
+function toRelativePaths(folder) {
+    return filename => {
+        return `../src/module/${folder}/${filename}`;
+    };
+}
+
+module.exports = Object.keys(filenames).reduce(toFilesList, []);
