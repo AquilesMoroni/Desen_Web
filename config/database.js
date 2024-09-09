448f140f8d76b2cef9b332b92f09fef760f80157
aquiles <eu@email.com>
2024-09-09 20:33:39 -0300

    atualization

--- config/database.js
+++ config/database.js
@@ -2,9 +2,10 @@ nodule.exports = {
     blog: {
         username: 'postgres',
         port:  '5432',
+        password:'postgres',
         database: 'blog',
         host: '127.0.0.1',
         dialect: 'postgres',
         logging: false
     }
-} 
+}
